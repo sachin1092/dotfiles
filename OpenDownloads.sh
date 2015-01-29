ce2896f18a93526bd7fd954505e945bd8356b0ac
sachin1092 <me@sachinshinde.com>
2015-01-29 18:40:41 +0530

    added restore script

--- OpenDownloads.sh
+++
@@ -1,8 +0,0 @@
-if grep -qs '/mnt/Downloads' /proc/mounts; then
-    echo "It's mounted."
-    nemo /mnt/Downloads/
-else
-    echo "It's not mounted."
-    sudo mount /dev/sda5
-    nemo /mnt/Downloads/
-fi
--- OpenMovies.sh
+++
@@ -1,8 +0,0 @@
-if grep -qs '/mnt/Movies\ \&\ Extras' /proc/mounts; then
-    echo "It's mounted."
-    nemo /mnt/Movies\ \&\ Extras/
-else
-    echo "It's not mounted."
-    sudo mount /dev/sda2
-    nemo /mnt/Movies\ \&\ Extras/
-fi
